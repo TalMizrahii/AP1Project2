44a4f40da357960a3f0ce8a66b3e35ed77c83bf8
TalMizrahii <Taltalon1927@gmail.com>
2022-12-07 18:58:05 +0200

    NOT TESTED reading a line method.

--- FileReader.cpp
+++ FileReader.cpp
@@ -4,6 +4,7 @@
 
 #include "FileReader.h"
 
+#define SIZE 13
 using namespace std;
 
 FileReader::FileReader() {
@@ -11,15 +12,44 @@ FileReader::FileReader() {
 }
 
 vector<vector<int>> FileReader::readFile(string &path) {
-    ifstream myFile(path);
-    if (!myFile.is_open()) {
+    ifstream myFile;
+    try{
+        // trying to open the file using the path.
+        myFile.open(path);
+        if (!myFile.is_open()) {
+            throw;
+        }
+    }
+    // Catching an error.
+    catch (...){
         cout << " NO FILE" << endl;
         exit(-1);
     }
 
-//    myFile.open(path);
-    string line = "";
-    while (getline(myFile, line)) {
+    // Declaring a vector of vectors.
+    vector<vector<double>> fileVec;
+    // While the file is still ok to read:
+    while (myFile.good()) {
+        // Create a new vector.
+        vector<double> dataVec;
+        // Read each data segment.
+        for (int i = 0; i < SIZE; ++i) {
+            // Read as a string.
+            string toNum;
+            // Put the data inside num.
+            getline(myFile, toNum, ',');
+            // Cast to double and put int the vector.
+            dataVec.push_back(stod(toNum));
+        }
+        // When finished with the line, put the vector in the all vector's vector.
+        fileVec.push_back(dataVec);
+    }
+
+    // Delete!!!!!!!!!!!! test only.
+    for (int i = 0; i < fileVec.size(); ++i) {
+        for (int j = 0; j < fileVec[i].size(); ++j) {
+            cout << fileVec[i][j] << endl;
+        }
     }
 }
 
